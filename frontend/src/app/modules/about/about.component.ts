1ba5798094f5f39844010e67b6ac3277385225c7
yqni13 <yqni13@protonmail.com>
2024-11-06 22:41:21 +0100

    feat: add basic navigation components
    Add first content of i18n .json translations.
    Add 'home', 'about' and 'service' component with basic structure.

---
+++ frontend/src/app/modules/about/about.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'tava-about',
+    templateUrl: './about.component.html',
+    styleUrl: './about.component.scss',
+    standalone: true,
+    imports: [
+
+    ]
+})
+export class AboutComponent {
+
+    constructor() {
+        //
+    }
+}
