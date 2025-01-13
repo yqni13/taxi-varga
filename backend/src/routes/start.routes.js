8fe57f682c6700c9ba889c88786142b9562051c9
yqni13 <yqni13@protonmail.com>
2025-01-13 23:49:00 +0100

    fix: fix spelling mistake + root endpoint message

--- backend/src/routes/start.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('', async (req, res) => {
-    res.send('taxi-varga api active!')
-});
-
-module.exports = router;
