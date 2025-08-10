7c63799bac27a18e7e8ddccb72f5a1aaf8a9ecf0
yqni13 <yqni13@protonmail.com>
2025-08-10 20:25:06 +0200

    refactor: check type of status in error middleware

--- backend/src/middleware/error.middleware.js
+++ backend/src/middleware/error.middleware.js
@@ -4,13 +4,17 @@ const { JWTExpirationException } = require('../utils/exceptions/auth.exception')
 
 function errorMiddleware(err, req, res, next) {
 
+    let status = err.status ? parseInt(err.status, 10) : 500;
+
     if((err.status === 500 || !err.message) && !err.isOperational) {
+        console.error('InternalServerException was assigned to Error');
         err = new InternalServerException('backend-500-server');
     } else if(err.status === 401 && err.message === 'jwt expired') {
+        console.error('JWTExpirationException was assigned to Error');
         err = new JWTExpirationException();
     }
 
-    const status = err.status && err.status >= 100 && err.status <= 599 ? err.status : 500;
+    status = err.status >= 100 && err.status <= 599 ? err.status : 500;
     const code = err.code || 'UNEXPECTED_ERROR';
     const message = err.message || 'Internal Server Error';
     const error = err.error || err.name || 'UnexpectedError';
