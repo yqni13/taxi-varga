63a22cffb59e4819621774f1766d976f03ceb4e6
yqni13 <yqni13@protonmail.com>
2026-04-25 22:09:08 +0200

    bug: init style + set @defer to balance rendering

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,6 +1,5 @@
-import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, inject, provideAppInitializer, provideZoneChangeDetection } from '@angular/core';
 import { PreloadAllModules, provideRouter, withPreloading } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { HttpClient, provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
@@ -8,6 +7,7 @@ import { CustomTranslateLoader } from '../../public/assets/i18n/custom-translate
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { appHttpInterceptor } from './app.http.interceptor';
 import { authInterceptorFn } from './app.auth.interceptor';
+import { ThemeHandlerService } from './shared/services/theme-handler.service';
 
 export function createTranslateLoader(http: HttpClient) {
   return new CustomTranslateLoader(http, './assets/i18n/', '.json');
@@ -16,6 +16,10 @@ export function createTranslateLoader(http: HttpClient) {
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideAppInitializer(() => {
+      const themeHandler = inject(ThemeHandlerService);
+      themeHandler.initTheme();
+    }),
     provideRouter(routes, withPreloading(PreloadAllModules)), 
     provideClientHydration(),
     provideHttpClient(
